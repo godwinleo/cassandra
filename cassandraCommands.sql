83f0dd5ee901b986e2ac929f6cc32551f2b77861
godwinleo <godwintheophilus251192@gmail.com>
2019-06-14 20:12:04 +0530

    added

---
+++ cassandraCommands.sql
@@ -0,0 +1,17 @@
+------------------------------
+disadvantages
+------------------------------
+
+- cosistency is a issue 2 nodes does not have same data for the same primary key
+
+- not build for updates
+
+- not read optimized, comparitively slower than rdbms
+
+- relations are not implicit means cant have foriegn keys
+
+- joins are not easy, can only be done using loops to compare data.
+
+------------------------------
+
+------------------------------
